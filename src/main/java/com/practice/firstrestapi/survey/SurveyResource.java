7ee1076fb9fef5c377b1e178412b7acca0e336cf
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-07-25 21:54:19 +0530

    created surveys

---
+++ src/main/java/com/practice/firstrestapi/survey/SurveyResource.java
@@ -0,0 +1,31 @@
+package com.practice.firstrestapi.survey;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class SurveyResource {
+    private SurveyService surveyService;
+
+    public SurveyResource(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @RequestMapping("/surveys")
+    public List<Survey> retrieveAllSurveys() {
+        return surveyService.retrieveAllSurveys();
+    }
+
+    @RequestMapping("/surveys/{surveyId}")
+    public Survey retrieveSurveyById(@PathVariable String surveyId) {
+        Survey survey = surveyService.retrieveSurveyById(surveyId);
+        if (survey == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return survey;
+    }
+}
