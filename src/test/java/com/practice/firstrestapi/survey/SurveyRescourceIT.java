41acfb33387a6f9a0c6c17daefc191d6f73947dc
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-07-27 18:52:54 +0530

    more integration tests added

--- src/test/java/com/practice/firstrestapi/survey/SurveyRescourceIT.java
+++ src/test/java/com/practice/firstrestapi/survey/SurveyRescourceIT.java
@@ -6,28 +6,17 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyRescourceIT {
-    String str = """
-            
-            {
-            "id": "Question1",
-            "description": "Most Popular Cloud Platform Today",
-            "options": [
-            "AWS",
-            "Azure",
-            "Google Cloud",
-            "Oracle Cloud"
-            ],
-            "correctAnswer": "AWS"
-            }
-            
-            """;
-
     private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+    private static String GENERIC_QUESTIONS_URL = "/surveys/Survey1/questions";
 
     @Autowired
     private TestRestTemplate template;
@@ -48,5 +37,53 @@ public class SurveyRescourceIT {
         assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
 
         JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+    }
+
+    @Test
+    void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTIONS_URL, String.class);
+        String expectedResponse =
+                """
+                        [
+                            {
+                                "id": "Question1"
+                            },
+                            {
+                                "id": "Question2"
+                            },
+                            {
+                                "id": "Question3"
+                            }
+                        ]
+                        """;
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+    }
+
+    @Test
+    void addNewSurveyQuestion_basicScenario() {
+        String requestBody = """
+                    {
+                    "id": "Question1",
+                    "description": "Most Popular Cloud Platform Today",
+                    "options":["AWS", "Azure", "Google Cloud", "Oracle Cloud"],
+                    "correctAnswer": "AWS"
+                    }
+                """;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+
+        ResponseEntity<String> responseEntity
+                = template.exchange(GENERIC_QUESTIONS_URL, HttpMethod.POST, httpEntity, String.class);
+
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+        String locationHeader = responseEntity.getHeaders().get("Location").get(0);
+        assertTrue(locationHeader.contains("/surveys/Survey1/questions/"));
+
+        template.delete(locationHeader);
     }
 }
